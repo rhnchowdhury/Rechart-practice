41e546e6addb22d00d182d53f115a731ea96a4c7
rhnchowdhury <rhnchdri@gmail.com>
2023-11-11 17:16:02 +0600

    with tooltip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Axis from './Components/AllAxis/Axis';
+import AxisTool from './Components/AllAxis/AxisTool';
 import Chart from './Components/Chart/Chart';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <h1 className='head-1'>Rechart implement for practice</h1>
      <Chart></Chart>
      <Axis></Axis>
+     <AxisTool></AxisTool>
     </div>
   );
 }
---
+++ src/Components/AllAxis/AxisTool.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './AxisTool.css';
+
+const AxisTool = () => {
+    const data = [
+        {
+            name: "Bengali",
+            marks: 70,
+            quiz: 24
+        },
+        {
+            name: "English",
+            marks: 55,
+            quiz: 20
+        },
+        {
+            name: "Math",
+            marks: 90,
+            quiz: 30
+        },
+        {
+            name: "Physics",
+            marks: 60,
+            quiz: 28
+        },
+        {
+            name: "Chemistry",
+            marks: 90,
+            quiz: 25
+        },
+        {
+            name: "Biology",
+            marks: 62,
+            quiz: 20
+        },
+        {
+            name: "Science",
+            marks: 75,
+            quiz: 25
+        }
+    ];
+    return (
+        <div className=''>
+            <h3>Axis With Tooltip</h3>
+            <div className='axis-tool'>
+                <LineChart width={600} height={400} data={data}>
+                    <Line type="monotone" dataKey="marks" stroke="#82ca9d" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+        </div>
+    );
+};
+
+export default AxisTool;
