6611289df6dcbe74f398756b090aab28a03658e2
rhnchowdhury <rhnchdri@gmail.com>
2023-11-11 15:55:22 +0600

    simple line add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Chart from './Components/Chart/Chart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='head-1'>Rechart implement for practice</h1>
+     <Chart></Chart>
     </div>
   );
 }
